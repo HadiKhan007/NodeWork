70904dcb05ecfea673059a91fd42dc3e8000ca50
Sohaib <alihaidercs17@gmail.com>
2023-11-05 17:21:46 +0500

    express/start

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
 
-const dirPath = path.join(__dirname, "Crud");
-const filePath = `${dirPath}/apple.txt`;
+const app = express();
 
-//   For Write File
-
-// fs.writeFileSync(filePath,"This is new File")
-
-//   For Read File
-
-// fs.readFile(filePath,"utf-8",(err,item)=>{
-// console.log(item)
-// })
-
-//    Append Text in File
-
-// fs.appendFile(filePath, " and FileName is apple.txt", (err) => {
-//   !err && console.log("file Updated");
-// });
-
-///   Rename File
-
-// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
-//   !err && console.log("file Updated");
-// });
-
-//    Delete File
-
-// fs.unlinkSync(`${dirPath}/fruit.txt`);
-
-let a = 20;
-let b = 0;
-
-const waitData = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(40);
-  }, 2000);
+app.get("", (req, res) => {
+  res.send(`
+  <h1>Welcome to Home Page</h1>
+  <a href='/About'>Go to About Page</a>
+  `);
 });
 
-waitData.then((data) => {
-  let b = data;
-  console.log(a + b);
+app.get("/About", (req, res) => {
+  res.send(
+    `
+    <input type='text' placeholder="Username" value="${req.query.name}"/>
+    <button>Click Me</button>
+    <a href='/'>Go to Home Page</a>
+
+    `
+  );
 });
+
+app.get("/Help", (req, res) => {
+  res.send([
+    {
+      name: "Ali",
+      email: "alihaidercs17@gmail.com",
+    },
+    {
+      name: "Haider",
+      email: "alihaidercs17@gmail.com",
+    },
+  ]);
+});
+
+app.listen(4000);
